b3a6da60d3d257a23b0afcdf801dc81d073667db
Cadence Eva Custin <eva.custin@gmail.com>
2023-07-24 11:50:37 -0400

    Reworked functions to use recursion and ternaries

--- js/site.js
+++ js/site.js
@@ -1,57 +1,74 @@
 // Gets the numbers from the value input fields
 function getValues() {
     // get the values from the form and attempt to convert string to number
-    let fizzNumber = Number(document.getElementById('fizz-value').value);
-    let buzzNumber = Number(document.getElementById('buzz-value').value);
-    let stopNumber = Number(document.getElementById('stop-value').value);
+    let fizz = Number(document.getElementById('fizz-value').value);
+    let buzz = Number(document.getElementById('buzz-value').value);
+    let start = Number(document.getElementById('start-value').value);
+    let end = Number(document.getElementById('end-value').value);
 
     // validation
-    if (isNaN(fizzNumber) == true || isNaN(buzzNumber) == true || isNaN(stopNumber) == true) {
-        // if we don't have numbers
+    if (isNaN(fizz) == true || isNaN(buzz) == true || isNaN(end) == true) {
         Swal.fire({
             title: 'Oops!',
             text: 'Sorry, but \'Fizz, B\' only works with real numbers',
             icon: 'error',
             backdrop: false
         });
-    } else if (stopNumber < 1) {
-        // if we have a stop value less than 1
+    } else if (start <= 0) {
+        Swal.fire({
+            title: 'Oops!',
+            text: 'Sorry, but start value must be greater than 0',
+            icon: 'error',
+            backdrop: false
+        });
+    } else if (end < 1) {
         Swal.fire({
             title: 'Oops!',
             text: 'Sorry, but no stop value less than 1',
             icon: 'error',
             backdrop: false
         });
-    } else if (stopNumber > 5000) {
-        // if we have a stop value greater than 5000
+    } else if (end > 5000) {
         Swal.fire({
             title: 'Oops!',
             text: 'Sorry, but no stop value greater than 5000',
             icon: 'error',
             backdrop: false
         });
+    } else if (start > end) {
+        Swal.fire({
+            title: 'Oops!',
+            text: 'Sorry, but start value can\'t be more than the end value',
+            icon: 'error',
+            backdrop: false
+        });
     } else {
-        // create an array of the numbers from 1 to stopNumber
-        let numbersToStyle = generateNumbers(stopNumber);
+        let numbersToStyle = FizzB(fizz, buzz, start, end);
 
-        // convert numbersToStyle into HTML and push it to the applet page
-        displayNumbers(fizzNumber, buzzNumber, numbersToStyle);
+        displayNumbers(numbersToStyle);
     }
 }
 
-function generateNumbers(stopNumber) {
-    let numbersToStyle = [];
+function FizzB(fizz, buzz, start, end) {
+    let value = '';
+    let currentArray = [];
 
-    // create an array of numbers from 1 up to stopNumber
-    for (let i = 1; i <= stopNumber; i++) {
-        numbersToStyle.push(i);
+    value += ((start % fizz == 0) ? 'fizz' : '') + ((start % buzz == 0) ? 'buzz' : '') + ((start % fizz != 0 && start % buzz != 0) ? start : '');
+    currentArray.push(value);
+    start++;
+
+    if (start > end) {
+        return currentArray;
+    } else {
+        let returnArray = FizzB(fizz, buzz, start, end);
+        returnArray.unshift(currentArray[0]);
+        currentArray = returnArray;
     }
-
-    return numbersToStyle;
+    return currentArray;
 }
 
 // Puts the numbers on the page
-function displayNumbers(fizz, buzz, numbersToStyle) {
+function displayNumbers(numbersToStyle) {
     let tableHtml = '';
     let currentHtmlString = '';
 
@@ -63,30 +80,14 @@ function displayNumbers(fizz, buzz, numbersToStyle) {
 
         // if the currentNumber is one greater than a number divisible by 5 
         // or otherwise the number 1, then begin a new row
-        if (currentNumber % 5 == 1) {
-            currentHtmlString = `<tr>`;
-        }
+        if ((i + 1) % 5 == 1) currentHtmlString = '<tr>';
 
-        if (currentNumber % fizz == 0 && currentNumber % buzz == 0) {
-            // if currentNumber is divisible by fizz *AND* buzz
-            currentHtmlString += '<td class = fizz-buzz>Fizz, B!</td>'
-        } else if (currentNumber % fizz == 0) {
-            // if currentNumber is divisible by *ONLY* fizz
-            currentHtmlString += '<td class = fizz>Fizz</td>'
-        } else if (currentNumber % buzz == 0) {
-            // if currentNumber is divisible by *ONLY* buzz
-            currentHtmlString += '<td class = buzz>B</td>'
-        } else {
-            // if currentNumber is divisible by *NEITHER* fizz nor buzz
-            currentHtmlString += `<td>${currentNumber}</td>`
-        }
+        if (currentNumber == 'fizzbuzz') {currentHtmlString += '<td class = fizz-buzz>'
+        } else if (currentNumber == 'fizz') {currentHtmlString += '<td class = fizz>'
+        } else if (currentNumber == 'buzz') {currentHtmlString += '<td class = buzz>'
+        } else currentHtmlString += '<td>';
 
-        // if currentNumber is disivible by 5, then end the row
-        if (currentNumber % 5 == 0) {
-            currentHtmlString += `</tr>`;
-        }
-
-        // append the string the loop's been building to tableHtml 
+        currentHtmlString += ((i + 1) % 5 == 0) ? `${currentNumber}</td></tr>` : `${currentNumber}</td>`;
         tableHtml += currentHtmlString;
     }
 
